b8943677e8b835b1c11642b2bda56b4f621b94ee
jedireza <reza@akhavan.me>
2013-09-18 00:41:54 -0700

    thirteenth answer

---
+++ learnyounode/13-http-json-api-server.js
@@ -0,0 +1,31 @@
+var http = require('http'),
+    moment = require('moment'),
+    map = require('through2-map'),
+    url = require('url'),
+    fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  
+  var urlParts = url.parse(req.url, true);
+  var thisMoment = new moment(urlParts.query.iso);
+  
+  if ('/api/parsetime' === urlParts.pathname) {
+    res.write(JSON.stringify({
+      'hour': parseInt(thisMoment.format('H'), null),
+      'minute': parseInt(thisMoment.format('mm'), null),
+      'second': parseInt(thisMoment.format('ss'), null)
+    }));
+    res.end();
+  }
+  else if ('/api/unixtime' === urlParts.pathname) {
+    res.write(JSON.stringify({
+      'unixtime': thisMoment._d.getTime()
+    }));
+    res.end();
+  }
+  else {
+    res.end();
+  }
+})
+server.listen(8000);
