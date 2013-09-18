ddcfc5025c5a25a37c3a2de5b3467fde12ea1028
jedireza <reza@akhavan.me>
2013-09-18 00:12:53 -0700

    twelfth answer, added through2-map module

---
+++ learnyounode/12-http-uppercaserer.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    map = require('through2-map'),
+    fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+  if (req.method === 'POST') {
+    req.pipe(map(function (chunk) {
+      return chunk.toString().toUpperCase();
+    })).pipe(res);
+  }
+  else {
+    res.end();
+  }
+})
+server.listen(8000);
