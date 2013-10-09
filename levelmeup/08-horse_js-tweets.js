e0705efaa4fb9532e40d350a8ac623da48d1649a
jedireza <reza@akhavan.me>
2013-10-08 22:56:07 -0700

    eighth answer

---
+++ levelmeup/08-horse_js-tweets.js
@@ -0,0 +1,14 @@
+module.exports = function(db, date, callback) {
+  var tweets = [];
+  db.createReadStream({ start: date, end: date +'\xff' })
+    .on('error', function (err) {
+      callback(err);
+    })
+    .on('data', function(data){
+      tweets.push(data.value);
+    })
+    .on('end', function() {
+      callback(null, tweets);
+    })
+  ;
+};
