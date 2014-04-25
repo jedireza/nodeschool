d065177da871c6a7bbf4918f5c3a5329d5185a2a
jedireza <reza@akhavan.me>
2014-04-24 17:58:11 -0700

    fixed fourth answer

--- learnyounode/04-my-first-async-io.js
+++ learnyounode/04-my-first-async-io.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function(err, contents) {
   var lines = contents.split('\n');
-  console.log(lines.length);
+  console.log(lines.length - 1);
 });
