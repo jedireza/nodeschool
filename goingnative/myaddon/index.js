83d520454ae7c8b28407761196cf4ce6f169da5c
jedireza <reza@akhavan.me>
2015-07-19 21:36:40 -0700

    goingnative part seven

--- goingnative/myaddon/index.js
+++ goingnative/myaddon/index.js
@@ -1,6 +1,13 @@
 var bindings = require('bindings');
 var addon = bindings('myaddon');
 
+var interval = setInterval(function () {
+  process.stdout.write('.')
+}, 50)
+
 addon.delay(process.argv[2], function () {
-  console.log('Done!');
-});
+  clearInterval(interval)
+  console.log('Done!')
+})
+
+process.stdout.write('Waiting')
--- goingnative/myaddon/myaddon.cc
+++ goingnative/myaddon/myaddon.cc
@@ -5,18 +5,60 @@
 
 using namespace v8;
 
+// A worker class extending the NanAsyncWorker helper
+// class, a simple encapsulation of worker-thread
+// logic to make simple tasks easier
+
+class MyWorker : public NanAsyncWorker {
+ public:
+  // Constructor
+  MyWorker(NanCallback *callback, int delay)
+    : NanAsyncWorker(callback), delay(delay) {}
+  // Destructor
+  ~MyWorker() {}
+
+  // Executed inside the worker-thread.
+  // It is not safe to access V8, or V8 data structures
+  // here, so everything we need for input and output
+  // should go on `this`.
+  void Execute () {
+    // Asynchronous, non-V8 work goes here
+    #ifdef _WIN32
+      Sleep(delay);
+    #else
+      usleep(delay * 1000);
+    #endif
+  }
+
+  // Executed when the async work is complete
+  // this function will be run inside the main event loop
+  // so it is safe to use V8 again
+  void HandleOKCallback () {
+    NanScope();
+
+    // NanCallback#Call() does a NanMakeCallback() for us
+    callback->Call(0, NULL);
+  }
+
+ private:
+  int delay;
+};
+
 NAN_METHOD(Delay) {
   NanScope();
 
+  // get delay and callback
   int delay = args[0]->IntegerValue();
+  Local<Function> callback = args[1].As<Function>();
 
-  #ifdef _WIN32
-    Sleep(delay);
-  #else
-    usleep(delay * 1000);
-  #endif
+  // create NanCallback instance wrapping the callback
+  NanCallback* nanCallback = new NanCallback(callback);
 
-  NanMakeCallback(NanGetCurrentContext()->Global(), args[1].As<Function>(), 0, NULL);
+  // create a MyWorker instance, passing the callback and delay
+  MyWorker* worker = new MyWorker(nanCallback, delay);
+
+  // queue the worker instance onto the thread-pool
+  NanAsyncQueueWorker(worker);
 
   NanReturnUndefined();
 }
@@ -25,4 +67,4 @@ void Init(Handle<Object> exports) {
   exports->Set(NanNew("delay"), NanNew<FunctionTemplate>(Delay)->GetFunction());
 }
 
-NODE_MODULE(myaddon, Init);
+NODE_MODULE(myaddon, Init)
